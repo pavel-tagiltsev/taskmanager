d52870b85d1613b7523456a627985a17bbac3db2
Павел <pavel.tagiltsef@gmail.com>
2021-08-10 15:25:13 +0300

    5.1.8 Отрефакторит логику кнопки допоказа
    - Локальные переменные перенесем в свойства класса, чтобы к ним был доступ отовсюду
    - Обработчик клика вынесем в отдельный метод
    - Пробег по массиву задач с последующим рендерингом заменим на уже описанный метод _renderTasks

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -12,18 +12,19 @@ const TASK_COUNT_PER_STEP = 8;
 export default class Board {
   constructor(boardContainer) {
     this._boardContainer = boardContainer;
+    this._renderedTaskCount = TASK_COUNT_PER_STEP;
 
     this._boardComponent = new BoardView();
     this._sortComponent = new SortView();
     this._taskListComponent = new TaskListView();
     this._noTaskComponent = new NoTaskView();
+    this._loadMoreButtonComponent = new LoadMoreButtonView();
+
+    this._handleLoadMoreButtonClick = this._handleLoadMoreButtonClick.bind(this);
   }
 
   init(boardTasks) {
     this._boardTasks = boardTasks.slice();
-    // Метод для инициализации (начала работы) модуля,
-    // малая часть текущей функции renderBoard в main.js
-
     render(this._boardContainer, this._boardComponent, RenderPosition.BEFOREEND);
     render(this._boardComponent, this._taskListComponent, RenderPosition.BEFOREEND);
 
@@ -31,15 +32,10 @@ export default class Board {
   }
 
   _renderSort() {
-    // Метод для рендеринга сортировки
-
     render(this._boardComponent, this._sortComponent, RenderPosition.AFTERBEGIN);
   }
 
   _renderTask(task) {
-    // Метод, куда уйдёт логика созданию и рендерингу компонетов задачи,
-    // текущая функция renderTask в main.js
-
     const taskComponent = new TaskView(task);
     const taskEditComponent = new TaskEditView(task);
 
@@ -73,40 +69,28 @@ export default class Board {
   }
 
   _renderTasks(from, to) {
-    // Метод для рендеринга N-задач за раз
-
     this._boardTasks
       .slice(from, to)
       .forEach((boardTask) => this._renderTask(boardTask));
   }
 
   _renderNoTasks() {
-    // Метод для рендеринга заглушки
-
     render(this._boardComponent, this._noTaskComponent, RenderPosition.AFTERBEGIN);
   }
 
+  _handleLoadMoreButtonClick() {
+    this._renderTasks(this._renderedTaskCount, this._renderedTaskCount + TASK_COUNT_PER_STEP);
+    this._renderedTaskCount += TASK_COUNT_PER_STEP;
+
+    if (this._renderedTaskCount >= this._boardTasks.length) {
+      remove(this._loadMoreButtonComponent);
+    }
+  }
+
   _renderLoadMoreButton() {
-    // Метод, куда уйдёт логика по отрисовке компонетов задачи,
-    // текущая функция renderTask в main.js
+    render(this._boardComponent, this._loadMoreButtonComponent, RenderPosition.BEFOREEND);
 
-    let renderedTaskCount = TASK_COUNT_PER_STEP;
-
-    const loadMoreButtonComponent = new LoadMoreButtonView();
-
-    render(this._boardComponent, loadMoreButtonComponent, RenderPosition.BEFOREEND);
-
-    loadMoreButtonComponent.setClickHandler(() => {
-      this._boardTasks
-        .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
-        .forEach((boardTask) => this._renderTask(boardTask));
-
-      renderedTaskCount += TASK_COUNT_PER_STEP;
-
-      if (renderedTaskCount >= this._boardTasks.length) {
-        remove(loadMoreButtonComponent);
-      }
-    });
+    this._loadMoreButtonComponent.setClickHandler(this._handleLoadMoreButtonClick);
   }
 
   _renderTaskList() {
@@ -118,16 +102,12 @@ export default class Board {
   }
 
   _renderBoard() {
-    // Метод для инициализации (начала работы) модуля,
-    // бОльшая часть текущей функции renderBoard в main.js
-
     if (this._boardTasks.every((task) => task.isArchive)) {
       this._renderNoTasks();
       return;
     }
 
     this._renderSort();
-
     this._renderTaskList();
   }
 }
